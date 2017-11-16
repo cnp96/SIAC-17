e3ff116025ac526bc34606b09f4952f669f05bba
Chinmaya Pati <chinmaya.cp@gmail.com>
2017-11-16 07:45:35 +0000

    Added Signup and Update

--- js/custom.js
+++ js/custom.js
@@ -61,3 +61,58 @@ function validateSignup(){
         //window.setTimeout(function(){ $("#process-label").css("display", "none"); }, 2500);
     }
 }
+
+function validateLogin() {
+    $("#li-process-label").css("display","none");
+    
+    var email=$("#li-inp-email").val(), pwd=$("#li-inp-pwd").val();
+    var validemail=false, validpwd=false;
+    
+    //email
+    var patt = new RegExp("^[a-z][a-z0-9\.\-\_\+]{2,20}@(gmail|yahoo|outlook|hotmail|mail).(com|in|org|xyz|co)$");
+    validemail = patt.test(email);
+    
+    //pwd
+    var patt = new RegExp("[A-Za-z0-9\@\#\-\_\+\=\*]{6,15}");
+    validpwd = patt.test(pwd);
+    
+    if(validemail && validpwd) {
+        $("#li-process-label").css({"display":"block", "color":"#3F51B5"}).html("Authenticating.. Please wait");
+        $.ajax({
+           type: "post",
+           url: "login.php",
+           data: {"email":email, "pwd":pwd},
+           statusCode: {
+               500: function(){ $("#li-process-label").css("color", "tomato").html("Internal Server Error. Try after sometime."); }
+           },
+           success: function(res) {
+               if(res==1 || res==0) {
+                   window.location.href = "dashboard.php";
+               } else {
+                   $("#li-process-label").css("color","tomato").html(res);
+               }
+           },
+           error: function(res) {
+               
+           }
+        });
+    } else {
+        $("#li-process-label").css({"display":"block", "color":"tomato"}).html("Invalid Credentials.");
+    }
+}
+
+function update() {
+    $("#loading").css("display","inline-block");
+    $.ajax({
+       type: "get",
+       url: "update.php",
+       success: function(res) {
+           res = jQuery.parseJSON(res);
+           console.log(res);
+           $("#loading").css("display","none");
+       },
+       error: function(err) {
+           $("#loading").css("display","none");
+       }
+    });
+}
